2c3e1ba2567f7b4d95ed673241b26cbfdd4fff59
Nauman-arshad483 <nauman.arshad483@gmail.com>
2024-01-12 04:12:54 +0500

    loader apge added

---
+++ src/app/components/EarthMesh.tsx
@@ -0,0 +1,51 @@
+// EarthMesh.tsx
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useRef, useEffect } from "react";
+import { Mesh } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import styles from "@/app/ui/loader.module.css";
+
+function Earth() {
+  const fileUrl = "earth_globe.glb";
+  const mesh = useRef<Mesh>(null!);
+  const gltf = useLoader(GLTFLoader, fileUrl);
+
+  useEffect(() => {
+    if (mesh.current) {
+      mesh.current.scale.set(1.5, 1.5, 1.5);
+      mesh.current.position.setY(17.4); 
+      mesh.current.position.setX(0);
+    }
+  }, []);
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  );
+}
+
+export function EarthMesh() {
+  return (
+    <div className={`${styles.EarthMesh}`}>
+      <div className={`${styles.EarthMeshContainer} ${styles.AlignTop}`}>
+        <Canvas
+          className={`${styles.EarthMeshContainerSub}`}
+          camera={{ position: [0, 0, 30] }} 
+        >
+          <OrbitControls />
+          <ambientLight />
+          <pointLight position={[0, 0, 5]} />
+          <Earth />
+        </Canvas>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,73 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import styles from "@/app/ui/loader.module.css";
+import { lusitana } from "./ui/fonts";
+import { EarthMesh } from "./components/EarthMesh";
 
 export default function Home() {
+  // State to manage loading completion
+  const [loadingComplete, setLoadingComplete] = useState(false);
+
+  // Function to handle loading completion
+  const handleLoadingComplete = () => {
+    setLoadingComplete(true);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={`${styles.layout_main}`}>
+      <div className={`${styles.section1}`}>
+        <h1
+          className={`mb-4 text-center  text-6xl font-bold ${lusitana.className}`}
+        >
+          About MAURITIUS
+        </h1>
+        <h2 className="text-xl font-semibold">
+          Welcome to Mauritius{" "}
+          <span
+            style={{
+              background: "linear-gradient(45deg, #ff8a00, #e52e71)",
+              padding: "5px 10px",
+              borderRadius: "5px",
+              fontSize: "2rem",
+              fontWeight: "bold",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Hubair
+          </span>
+        </h2>
+        <p className="text-[linear-gradient(45deg, #ff8a00, #e52e71)] text-lg mt-4 tracking-wide">
+          The new way to travel.
+        </p>
+        <div className={`relative mt-4 w-full ${styles.progress_container}`}>
+          <progress
+            className={`w-full ${styles.progress_bar}`}
+            value={loadingComplete ? "100" : "50"} // Update value based on loading completion
+            max="100"
+          ></progress>
+          <span
+            className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform ${styles.progress_text}`}
+          >
+            LOADING
+          </span>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={`${styles.section2}`}>
+        <div className={`flex ${styles.earth_container}`}>
+          <EarthMesh />
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* Render the video popup when loading is complete */}
+      {loadingComplete && (
+        <div className={`${styles.videoPopup}`}>
+          {/* Add your video component or any content here */}
+          <video controls width="400">
+            <source src="your-video-source.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      )}
     </main>
-  )
+  );
 }
